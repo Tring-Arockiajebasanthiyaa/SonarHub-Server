0237ddac1d515a18c540e66f91df93dfa468772f
tring-Arockiajebasanthiyaa <arockia.santhiya@tringapps.com>
2025-03-25 13:12:23 +0530

    Add missing files

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to: string, subject: string, text: string) => {
   const adminEmail = process.env.EMAIL_USER;
   if (!adminEmail) {
-    console.error("❌ Missing email credentials in .env");
+    console.error("Missing email credentials in .env");
     return;
   }
 
@@ -11,7 +11,6 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
     service: "gmail",
     auth: {
       user: adminEmail,
-     
     },
   });
 
@@ -24,8 +23,8 @@ export const sendEmail = async (to: string, subject: string, text: string) => {
 
   try {
     const info = await transporter.sendMail(mailOptions);
-    console.log(`✅ Email sent: ${info.response}`);
+    console.log(`Email sent: ${info.response}`);
   } catch (error) {
-    console.error("❌ Error sending email:", error);
+    console.error("Error sending email:", error);
   }
 };
